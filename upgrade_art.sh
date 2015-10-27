4580c2cad0318807509a91d1968125f9ec1eee46
guy-cohen <guyc@jfrog.com>
2015-10-27 09:19:02 +0200

    Testing Uploads

---
+++ upgrade_art.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   echo "USAGE: sudo bash upgrade_ver.sh -p $ARTIFACTORY_HOME"
+   exit 1
+fi
+
+echo "Is artifactory running as a service (Yes/No)?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) SERVICE_MODE=true; echo "y"; break;;
+        No ) SERVICE_MODE=false; echo "n"; break;;
+    esac
+done
+
+parse_options() {
+    while getopts "w:s:S:v:c:ChRdVunfrEGi:p:P" opt; do
+	   case $opt in
+        p)
+          ARTIFACTORY_HOME=$OPTARG
+          ;;
+        :)
+          echo "ERROR: Option -$OPTARG requires an argument." >&2
+          exit 1
+          ;;
+      esac
+    done
+}
+ 
+parse_options $@
+
+if [ -z "$ARTIFACTORY_HOME" ]; then
+    echo "Please specify your ARTIFACTORY_HOME using -p"
+    echo "Usage: bash upgrade_ver.sh -p ARTIFACTORY_HOME"
+    exit 1
+fi
+
+SERVERXML="$ARTIFACTORY_HOME/server.xml"
+WEBAPPS_ART="$ARTIFACTORY_HOME/webapps/artifactory.war"
+TOMCAT_DIR="$ARTIFACTORY_HOME/tomcat"
+BIN_DIR="$ARTIFACTORY_HOME/bin"
+NEW_VER_DIR="$(pwd)/"
+
+if [[ (! -f $NEW_VER_DIR/README.txt) || (! -d $NEW_VER_DIR/webapps) || (! -d $NEW_VER_DIR/bin) || (! -d $NEW_VER_DIR/etc) ]]; then
+  echo "ERROR: Please run the upgrade script from the new package directory"
+  exit 1
+fi
+
+if [ "$SERVICE_MODE" = true ] ; then
+  echo "Stopping the artifactory service"
+  service artifactory stop
+else
+  RESULT=$(ps -a | grep artifactory | wc -l)
+  if [ $RESULT -eq 0 ]; then
+      echo "Deploying new version"
+  else 
+      echo "Please stop the artifactory service before deploying the new version"
+      exit 1
+  fi
+fi
+
+echo "Using $ARTIFACTORY_HOME as ARTIFACTORY_HOME"
+
+cp "$SERVERXML" "/tmp"
+echo "Copied server.xml to a temporary location"
+
+echo "Start detleting old version"
+rm "$WEBAPPS_ART"
+echo "Deleted $WEBAPPS_ART"
+
+rm -r "$TOMCAT_DIR"
+echo "Deleted $TOMCAT_DIR" 
+
+rm -r "$BIN_DIR"
+echo "Deleted $BIN_DIR"
+
+echo "Start copying new version"
+cp -R "$NEW_VER_DIR/bin" "$ARTIFACTORY_HOME"
+echo "Copied $NEW_VER_DIR/bin to $ARTIFACTORY_HOME"
+
+cp -R "$NEW_VER_DIR/tomcat" "$ARTIFACTORY_HOME"
+echo "Copied $NEW_VER_DIR/tomcat to $ARTIFACTORY_HOME"
+
+cp "$NEW_VER_DIR/webapps/artifactory.war" "$ARTIFACTORY_HOME/webapps"
+echo "Copied $NEW_VER_DIR/webapps/artifactory.war to $ARTIFACTORY_HOME/webapps"
+
+cp "/tmp/server.xml" "$SERVERXML"
+echo "Copied server.xml back to $ARTIFACTORY_HOME"
+
+echo "Running new artifactory version"
+sudo /bin/bash "$ARTIFACTORY_HOME/bin/artifactory.sh"
+
+if [ "$SERVICE_MODE" = true ] ; then
+sudo /bin/bash "$ARTIFACTORY_HOME/bin/installService.sh"
+  echo "New artifactory is up and running"
+else
+sudo /bin/bash "$ARTIFACTORY_HOME/bin/artifactory.sh"
+  echo "New artifactory is up and running"
+fi
